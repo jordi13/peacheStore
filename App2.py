ef193f0dfb7117d7c8851b03c8345bfd4680d480
jordi13 <jordibs9@hotmail.com>
2014-05-16 17:50:27 +0200

    Añadir una descarga,comentario implementado

---
+++ App2.py
@@ -0,0 +1,76 @@
+__author__ = 'jordiblanchsalgado'
+
+class App2():
+    def __init__(self,nombre):
+        self.nombre = nombre
+
+    def getNombre(self):
+        return self.nombre
+
+
+    def sumarDescarga(self,nombre):
+        varNombre = nombre
+
+        with open('apps.txt',mode='r',encoding='utf-8')as archivo:
+            contenido = ""
+            contModificad = ""
+
+            for linia in archivo:
+                nombre,proveedor,fecha_publi,precio,num_descargas,num_puntuaciones,puntuacion,num_comentarios = linia.split(',',7)
+                num_comentarios = num_comentarios.strip("\n")
+
+                if varNombre != nombre:
+                    contenido = contenido + (nombre+","+proveedor+","+fecha_publi+","+str(precio)+","+str(num_descargas)+","+str(num_puntuaciones)+","+str(puntuacion)+","+str(num_comentarios)+"\n")
+
+                else:
+                    nuevo_num_descargas = int(num_descargas) + 1
+                    contModificad = (nombre+","+proveedor+","+fecha_publi+","+str(precio)+","+str(nuevo_num_descargas)+","+str(num_puntuaciones)+","+str(puntuacion)+","+str(num_comentarios)+"\n")
+
+            contTotal = contenido+contModificad
+
+        with open('apps.txt',mode='w',encoding='utf-8')as archivo:
+            archivo.write(contTotal)
+            print("")
+            print("---DESCARGA SUMADA---")
+            print("")
+
+    def crearApp(self,nombre):
+        varNombre = nombre
+        with open('apps.txt',mode='r',encoding='utf-8')as archivo:
+            encontrado = False
+            for linia in archivo:
+                nombre,proveedor,fecha_publi,precio,num_descargas,num_puntuaciones,puntuacion,num_comentarios = linia.split(',',7)
+                num_comentarios = num_comentarios.strip("\n")
+                if varNombre == nombre:
+                    encontrado = True
+                    app = App2(nombre)
+
+            if(encontrado == False):
+                print("No encontrada esta APP")
+        return app
+
+    def sumarComentari(self,nombre):
+        varNombre = nombre
+
+        with open('apps.txt',mode='r',encoding='utf-8')as archivo:
+            contenido = ""
+            contModificad = ""
+
+            for linia in archivo:
+                nombre,proveedor,fecha_publi,precio,num_descargas,num_puntuaciones,puntuacion,num_comentarios = linia.split(',',7)
+                num_comentarios = num_comentarios.strip("\n")
+
+                if varNombre != nombre:
+                    contenido = contenido + (nombre+","+proveedor+","+fecha_publi+","+str(precio)+","+str(num_descargas)+","+str(num_puntuaciones)+","+str(puntuacion)+","+str(num_comentarios)+"\n")
+
+                else:
+                    nuevo_num_comentarios = int(num_comentarios) + 1
+                    contModificad = (nombre+","+proveedor+","+fecha_publi+","+str(precio)+","+str(num_descargas)+","+str(num_puntuaciones)+","+str(puntuacion)+","+str(nuevo_num_comentarios)+"\n")
+
+            contTotal = contenido+contModificad
+
+        with open('apps.txt',mode='w',encoding='utf-8')as archivo:
+            archivo.write(contTotal)
+            print("")
+            print("---COMENTARIO SUMADA---")
+            print("")
--- main.py
+++ main.py
@@ -1,3 +1,62 @@
 __author__ = 'jordiblanchsalgado'
-def blabal():
-    print()
+
+import App2
+
+def menu():
+    print("BIENVENIDO A LA PEACHSTORE")
+    print("1-. Añadir nueva app")
+    print("2-. Sumar una descarga")
+    print("3-. Sumar un comentario")
+    print("4-. Modificar los datos de identificacion de una APP")
+    print("5-. Mostrar lista Apps ordenadas (Gratis/Pago)")
+    print("6-. Calcular el num de estrellas de una APP")
+    print("7-. Calcular el importe total ingresado hasta el momento (solo de pago)")
+    print("8-. Listar Apps de un mismo proveedor ")
+    print("9-. SALIR")
+
+eleccion = 0
+menu()
+while eleccion != 9:
+    eleccion = int(input())
+
+    #AÑADIR NUEVA APP
+    if eleccion == 1:
+        print("")
+        print("AÑADIR NUEVA APP: ")
+        print("")
+        print("Nombre :")
+        nombreApp = str(input())
+        print("Proveedor :")
+        proveedor = str(input())
+        print("Fecha de publicacion :")
+        fecha = str(input())
+        print("Precio :")
+        precio = float(input())
+        num_descargas = "0"
+        num_puntuaciones = "0"
+        puntuacion = "0"
+        num_comentarios = "0"
+
+        #Escribir apps.txt
+        with open('apps.txt', mode='a', encoding='utf-8')as archivo:
+                archivo.write(nombreApp+","+proveedor+","+fecha+","+str(precio)+","+num_descargas+","+num_puntuaciones+","+puntuacion+","+num_comentarios+"\n")
+        print("")
+        print("App añadida")
+        print("")
+        menu()
+
+    #SUMAR UNA DESCARGA
+    if eleccion == 2:
+        print("Itroduce el nombre de la app ")
+        nombre = str(input())
+        apps = App2.App2(nombre)
+        apps.crearApp(nombre)
+        apps.sumarDescarga(nombre)
+        menu()
+    if eleccion == 3:
+        print("Introduce el nombre de la app ")
+        nombre = str(input())
+        apps = App2.App2(nombre)
+        apps.crearApp(nombre)
+        apps.sumarComentari(nombre)
+        menu()
